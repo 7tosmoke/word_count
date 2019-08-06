efe75698e9466194271aaa202e2fb8aa2338ffaa
7tosmoke <929434413@qq.com>
2019-08-06 13:20:02 +0800

    添加了源代码文件

---
+++ Django/function.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    text = request.GET['text']
+    total_count = len(request.GET['text'])
+
+    word_dict = {}
+
+    for word in text:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+    sorted_dict =\
+        sorted(word_dict.items(), key=lambda w: w[1], reverse=True)
+
+    return render(request, 'count.html',
+                  {'count': total_count, 'text': text,
+                   'sorted': sorted_dict})
+
